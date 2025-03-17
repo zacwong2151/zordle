e1fc71f268a80e5d05dacd42f1410cc6b663c5a9
zacwong2151 <zacwong2151@gmail.com>
2025-03-17 12:18:18 +0800

    Implement Player APIs for battleService

---
+++ battleService/src/routes/playerRoutes.ts
@@ -0,0 +1,109 @@
+import { Router, Request, Response } from "express";
+import { Player } from "../models/Player";
+
+const DUPLICATE_KEY_CODE = 11000
+
+const router = Router()
+
+/**
+ * Get player
+ */
+router.get("/:email", async (req: Request, res: Response) => {
+    const email = req.params.email
+
+    if (!email) {
+        res.status(400).send({
+            data: "Missing information"
+        })
+        return
+    }
+
+    try {
+        const player = await Player.findOne({ _id: email })
+
+        if (player) {
+            res.status(200).send({
+                data: player,
+            })
+        } else {
+            res.status(404).send({
+                data: null,
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            data: "Internal server error"
+        })
+    }
+})
+
+/**
+ * Create player
+ */
+router.post("/", async (req: Request, res: Response) => {
+    const { email, roomId } = req.body
+
+    if (!email || !roomId) {
+        res.status(400).send({
+            data: "Missing information"
+        })
+        return
+    }
+
+    const player = new Player({
+        _id: email,
+        roomId: roomId
+    })
+
+    try {
+        await player.save()
+        res.status(201).send({
+            data: player
+        })
+    } catch (error: any) {
+        if (error.code === DUPLICATE_KEY_CODE) {
+            res.status(409).send({
+                data: `Player with email: ${email} already exists` 
+            })
+        } else {
+            res.status(500).send({
+                data: "Internal server error "
+            })
+        }
+    }
+})
+
+/**
+ * Delete player
+ */
+router.delete("/:email", async (req: Request, res: Response) => {
+    const email = req.params.email
+
+    if (!email) {
+        res.status(400).send({
+            data: "Missing information"
+        })
+        return
+    }
+
+    try {
+        const player = await Player.findOneAndDelete({ _id: email })
+
+        if (player) {
+            res.status(200).send({
+                data: `Successfully delete player: ${email}`,
+            })
+        } else {
+            res.status(404).send({
+                data: `Player with email: ${email} does not exist`,
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            data: "Internal server error "
+        })
+        console.error(error)
+    }
+})
+
+export default router
--- battleService/src/server.ts
+++ battleService/src/server.ts
@@ -2,18 +2,19 @@ import express, { Express, Request, Response, Application } from 'express';
 import cors from "cors"
 import mongoose from 'mongoose';
 import { MONGO_CONNECTION_URL } from "../config"
-import { Player } from './models/Player';
+import router from './routes/playerRoutes';
 
 const app: Application = express();
 const corsOptions = {
-    origin: 'http://localhost:5173',
-    optionsSuccessStatus: 200,
-    credentials: true
+    origin: ["http://localhost:5173", "https://zordle.fly.dev"], // Allow requests from dev and prod frontends
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, // If using cookies or authentication
 }
 const PORT = 7000;
 
 app.use(express.json());
 app.use(cors(corsOptions))
+app.use("/battle", router)
 
 app.listen(PORT, () => {
     console.log(`battleService is running on port ${PORT}`);
@@ -22,8 +23,3 @@ app.listen(PORT, () => {
 mongoose.connect(MONGO_CONNECTION_URL)
     .then(() => console.log("Connected to MongoDB via Mongoose!"))
     .catch((err) => console.error("MongoDB connection error:", err))
-
-const player = new Player({
-    _id: "123",
-    roomId: "qewrty"
-})
